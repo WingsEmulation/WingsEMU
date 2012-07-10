2f7981306b0bc0b8e1183de476d0cf71906ebf78
Shutok Hanekoma <shutok2@hotmail.com>
2012-07-10 09:46:24 +0200

    Core/Scrips: Gluth [Regigicas]

---
+++ sql/WingsDB/gluth.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET faction_A = '1965', faction_H = '1965' WHERE entry IN (15932, 29417);
+UPDATE creature_template SET faction_A = '1693', faction_H = '1693' WHERE entry IN (16360, 30303);
+UPDATE creature_template SET ScriptName = 'npc_gluth_zombie' WHERE entry = 16360;
